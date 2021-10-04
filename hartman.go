eef5c71fb5059ae48da182cf0b754b4396ba9207
Seshchalam Malisetti <s.malisetti@accenture.com>
2021-10-04 11:31:08 +0530

    remove supervisor add RunN func

--- hartman.go
+++ hartman.go
@@ -1,102 +1,17 @@
-// Package hartman supervises and keeps n workers at all times. https://en.wikipedia.org/wiki/Full_Metal_Jacket
+// Package hartman.
+// https://en.wikipedia.org/wiki/Full_Metal_Jacket
 package hartman
 
 import (
 	"context"
-	"errors"
-	"log"
 	"sync"
 )
 
-// ErrAllDone lets supervisor that to complete a worker
-var ErrAllDone = errors.New("no more work")
-
-// Worker defines Work func
-type Worker interface {
-	Work(ctx context.Context) error
-}
-
-// Supervisor supervises the workers
-type Supervisor struct {
-	sync.Mutex
-	errors chan error
-
-	Ctx             context.Context
-	NumberOfWorkers int // number of workers
-	Worker          Worker
-	ErrorHandler    func(errors <-chan error)
-	DoneHandler     func()
-}
-
-// NewSupervisor creates Supervisor
-func NewSupervisor(ctx context.Context, numWorkers int, worker Worker) *Supervisor {
-	return &Supervisor{
-		Ctx:             ctx,
-		NumberOfWorkers: numWorkers,
-		Worker:          worker,
-	}
-}
-
-// SetErrorHandler sets a error handler and runs it in a routine
-func (s *Supervisor) SetErrorHandler(errHandler func(errors <-chan error)) {
-	s.Lock()
-	defer s.Unlock()
-
-	s.ErrorHandler = errHandler
-}
-
-// SetDoneHandler sets a done handler func which is called when supervision is done
-func (s *Supervisor) SetDoneHandler(doneHandler func()) {
-	s.Lock()
-	defer s.Unlock()
-
-	s.DoneHandler = doneHandler
-}
-
-// Work implements Worker
-func (s *Supervisor) Work(ctx context.Context) error {
-	s.errors = make(chan error)
-	defer close(s.errors)
-
-	if s.ErrorHandler == nil {
-		s.ErrorHandler = func(errors <-chan error) {
-			for err := range errors {
-				log.Printf("supervisor received error with: %v", err)
-			}
-		}
-	}
-
-	go s.ErrorHandler(s.errors)
-
-	var wg sync.WaitGroup
-	for i := 0; i < s.NumberOfWorkers; i++ {
-		wg.Add(1)
-		// start workers
-		go func(ctx context.Context) {
-			defer wg.Done()
-			for ctx.Err() == nil {
-				switch err := s.Worker.Work(ctx); err {
-				case nil:
-					/* nop */
-				case ErrAllDone:
-					return
-				default:
-					s.errors <- err
-				}
-			}
-		}(ctx)
-	}
-	wg.Wait()
-
-	if s.DoneHandler != nil {
-		s.DoneHandler()
-	}
-
-	return nil
-}
+// Work is func
+type Work = func(ctx context.Context) error
 
 // RunC runs fns concurrently, finishes if any worker errors
-func RunC(ctx context.Context, fns ...Worker) error {
+func RunC(ctx context.Context, fns ...Work) error {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
@@ -110,9 +25,9 @@ func RunC(ctx context.Context, fns ...Worker) error {
 
 	for _, fn := range fns {
 		wg.Add(1)
-		go func(fn Worker) {
+		go func(fn Work) {
 			defer wg.Done()
-			err := fn.Work(ctx)
+			err := fn(ctx)
 
 			if err != nil {
 				errors <- err
@@ -128,35 +43,64 @@ func RunC(ctx context.Context, fns ...Worker) error {
 }
 
 // RunF runs fns concurrently, finishes if any worker successfully completes
-func RunF(ctx context.Context, fns ...Worker) error {
+func RunF(ctx context.Context, fns ...Work) error {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	done := make(chan error)
+	errors := make(chan error)
 	var wg sync.WaitGroup
 
 	go func() {
 		wg.Wait()
-		close(done)
+		close(errors)
 	}()
 
 	for _, fn := range fns {
 		wg.Add(1)
-		go func(fn Worker) {
+		go func(fn Work) {
 			defer wg.Done()
-			err := fn.Work(ctx)
+			err := fn(ctx)
 
 			if err == nil {
-				done <- nil
+				errors <- nil
 			}
 		}(fn)
 	}
 
-	return <-done
+	return <-errors
+}
+
+// RunN runs given fn concurrently on n go routines, finishes if any error from a routine or when all completes
+func RunN(ctx context.Context, fn Work, n int) error {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	errors := make(chan error)
+	defer close(errors)
+
+	var wg sync.WaitGroup
+	go func() {
+		wg.Wait()
+		close(errors)
+	}()
+
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func(ctx context.Context) {
+			defer wg.Done()
+			err := fn(ctx)
+			if err != nil {
+				errors <- err
+				cancel()
+			}
+		}(ctx)
+	}
+
+	return <-errors
 }
 
 // RunS runs fns concurrently, restarts any fn that completes untill ctx is cancelled
-func RunS(ctx context.Context, fns ...Worker) <-chan error {
+func RunS(ctx context.Context, fns ...Work) <-chan error {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
@@ -169,10 +113,10 @@ func RunS(ctx context.Context, fns ...Worker) <-chan error {
 
 	for _, fn := range fns {
 		wg.Add(1)
-		go func(fn Worker) {
+		go func(fn Work) {
 			defer wg.Done()
 			for ctx.Err() == nil {
-				err := fn.Work(ctx)
+				err := fn(ctx)
 				if err != nil {
 					errors <- err
 				}
--- hartman_test.go
+++ hartman_test.go
@@ -5,107 +5,9 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
-	"strconv"
 	"testing"
 	"time"
 )
-
-type LazyWorker struct {
-}
-
-func (w *LazyWorker) Work(ctx context.Context) error {
-	timer := time.NewTimer(1 * time.Second)
-	for {
-		select {
-		case <-time.After(2 * time.Second):
-			return fmt.Errorf("Done my work")
-		case <-ctx.Done():
-			return nil
-		case <-timer.C:
-			time.Sleep(1 * time.Second)
-		}
-	}
-}
-
-func TestSupervise(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	ti := time.AfterFunc(20*time.Second, func() {
-		cancel()
-	})
-	defer ti.Stop()
-
-	numWorkers := 2
-	s := NewSupervisor(ctx, numWorkers, &LazyWorker{})
-
-	s.SetDoneHandler(func() {
-		log.Println("Done supervising")
-	})
-
-	s.SetErrorHandler(func(errors <-chan error) {
-		for err := range errors {
-			log.Printf("in error handler %v\n", err)
-		}
-	})
-
-	err := s.Work(ctx)
-	if err != nil {
-		t.Error(err)
-	}
-}
-
-type LazyWorker2 struct {
-	WorkChan chan string
-}
-
-func (w *LazyWorker2) Work(ctx context.Context) error {
-	for {
-		select {
-		case s, ok := <-w.WorkChan:
-			if !ok {
-				return ErrAllDone
-			}
-
-			time.Sleep(2 * time.Second)
-			log.Println("got work", s)
-		case <-ctx.Done():
-			return nil
-		}
-	}
-}
-
-func TestSupervise2(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	ti := time.AfterFunc(20*time.Second, func() {
-		cancel()
-	})
-	defer ti.Stop()
-
-	numWorkers := 2
-	workChan := make(chan string)
-	go func() {
-		defer close(workChan)
-		for i := 0; i < 20; i++ {
-			workChan <- strconv.Itoa(i)
-		}
-	}()
-
-	s := NewSupervisor(ctx, numWorkers, &LazyWorker2{WorkChan: workChan})
-
-	s.SetDoneHandler(func() {
-		log.Println("Done supervising")
-	})
-
-	s.SetErrorHandler(func(errors <-chan error) {
-		for err := range errors {
-			log.Printf("in error handler %v\n", err)
-		}
-	})
-
-	err := s.Work(ctx)
-	if err != nil {
-		t.Error(err)
-	}
-}
 
 /**
 	Will try make sandwich
@@ -114,16 +16,7 @@ func TestSupervise2(t *testing.T) {
 	need waiter
 **/
 
-type TShopper struct {
-}
-
-type CShopper struct {
-}
-
-type SWaiter struct {
-}
-
-func (w *TShopper) Work(ctx context.Context) error {
+func TShopper(ctx context.Context) error {
 	select {
 	case <-time.After(5 * time.Second):
 		log.Println("shopped tomatoes")
@@ -133,12 +26,12 @@ func (w *TShopper) Work(ctx context.Context) error {
 	}
 }
 
-func (w *CShopper) Work(ctx context.Context) error {
+func CShopper(ctx context.Context) error {
 	select {
 	case <-time.After(4 * time.Second):
 		r := rand.New(rand.NewSource(time.Now().UnixNano()))
 		if r.Intn(100)%2 == 0 {
-			return fmt.Errorf("cheese is not available. sorry, no sanwich")
+			return fmt.Errorf("cheese is not available. sorry, no sandwich")
 		}
 		log.Println("shopped cheese")
 		return nil
@@ -147,7 +40,7 @@ func (w *CShopper) Work(ctx context.Context) error {
 	}
 }
 
-func (w *SWaiter) Work(ctx context.Context) error {
+func SWaiter(ctx context.Context) error {
 	select {
 	case <-time.After(10 * time.Second):
 		log.Printf("sandwich is ready")
@@ -161,7 +54,7 @@ func TestRunC(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
 
-	err := RunC(ctx, &TShopper{}, &CShopper{}, &SWaiter{})
+	err := RunC(ctx, TShopper, CShopper, SWaiter)
 
 	if err != nil {
 		t.Error(err)
